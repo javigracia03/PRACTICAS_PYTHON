46f9fd7fa4e82d7df62b3eaba183a1304c3735fe
Javier Gracia <javiergracia@MacBook-Pro-de-Javier.local>
2023-02-06 21:42:31 +0100

    Ejercicios 5, 6 y 7

---
+++ PRACTICA_1/ejercicio3.py
@@ -0,0 +1,3 @@
+nombre = input("Como te llamas?")
+
+print("¡Hola", nombre, "!")
---
+++ PRACTICA_1/ejercicio5.py
@@ -0,0 +1,5 @@
+horas = float(input("¿Cuantas horas has trabajado?"))
+
+coste = float(input("¿Cuánto cuesta cada hora?"))
+
+print("La paga que le corresponde es de", coste*horas, "€")
